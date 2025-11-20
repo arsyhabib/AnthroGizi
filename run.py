a678299cc65e3d8cc2e1edb226c16351e63b84d5
arsyhabib <habibarsy401@gmail.com>
2025-11-20 22:17:15 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import os
 from app import app
 
 if __name__ == '__main__':
+    # Mengambil port dari environment variable Render (default 5000)
     port = int(os.environ.get('PORT', 5000))
-    # Gunakan setting debug=False untuk production
+    
+    # Menjalankan aplikasi
+    # debug=False penting untuk production agar error tidak tampil di browser
     app.run(host='0.0.0.0', port=port, debug=False)
