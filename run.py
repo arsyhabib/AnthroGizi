5b8de25c86ca1a32d44b53f1e79cd5498fbd4ca4
arsyhabib <habibarsy401@gmail.com>
2025-11-21 08:48:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python3
-import os
 from app import app
-
-if __name__ == '__main__':
-    # Mengambil port dari environment variable Render (default 5000)
-    port = int(os.environ.get('PORT', 5000))
-    
-    # Menjalankan aplikasi
-    # debug=False penting untuk production agar error tidak tampil di browser
-    app.run(host='0.0.0.0', port=port, debug=False)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
